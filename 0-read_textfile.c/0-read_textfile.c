0b6c3947e46c1da8ceafd5113064ad2c022a2ecd
Karimkrawes21 <abdelkarimboujarra1@gmail.com>
2023-10-02 10:01:49 -0700

    1

--- 0-read_textfile.c/0-read_textfile.c
+++ 0-read_textfile.c/0-read_textfile.c
@@ -7,14 +7,12 @@ char *buf;
 ssize_t fd;
 ssize_t w;
 ssize_t t;
-
 fd = open(filename, O_RDONLY);
 if (fd == -1)
 return (0);
 buf = malloc(sizeof(char) * letters);
 t = read(fd, buf, letters);
 w = write(STDOUT_FILENO, buf, t);
-
 free(buf);
 close(fd);
 return (w);
