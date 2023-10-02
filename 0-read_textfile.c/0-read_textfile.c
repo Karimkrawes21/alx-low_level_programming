8040cc5b781081578c17462eff0848cd2ca92ecf
Karimkrawes21 <abdelkarimboujarra1@gmail.com>
2023-10-02 10:08:46 -0700

    1

--- 0-read_textfile.c/0-read_textfile.c
+++ 0-read_textfile.c/0-read_textfile.c
@@ -1,17 +1,29 @@
 #include "main.h"
 #include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 char *buf;
-ssize_t fd;
-ssize_t w;
-ssize_t t;
+int fd;
+ssize_t t, w;
 fd = open(filename, O_RDONLY);
 if (fd == -1)
 return (0);
 buf = malloc(sizeof(char) * letters);
+if (buf == NULL)
+{
+close(fd);
+return (0);
+}
 t = read(fd, buf, letters);
+if (t == -1)
+{
+free(buf);
+close(fd);
+return (0);
+}
 w = write(STDOUT_FILENO, buf, t);
 free(buf);
 close(fd);
